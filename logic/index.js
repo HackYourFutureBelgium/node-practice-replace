e2c8b9922ebeb409e5599fb065867914f9712c3c
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2021-08-02 17:49:45 +0200

    little bit more robust

--- logic/index.js
+++ logic/index.js
@@ -1,5 +1,14 @@
+'use strict';
+
+/**
+ *
+ * @param {string} text - the text to search
+ * @param {string} toReplace - the thing to replace
+ * @param {string} withThis - what to replace it with
+ * @returns {string} the text with the thing replaced with the other thing
+ */
 const replace = (text, toReplace, withThis) => {
-  // write me!
+  return 'write me!';
 };
 
 module.exports = replace;
--- test/index.js
+++ test/index.js
@@ -1,29 +1,45 @@
 const replace = require('../logic/index.js');
 const fs = require('fs');
+const path = require('path');
 
-const tests = fs.readdirSync(__dirname, { withFileTypes: true })
+const tests = fs
+  .readdirSync(__dirname, { withFileTypes: true })
   .filter(item => item.isDirectory())
   .map(dir => require(__dirname + '/' + dir.name));
 
-const testResults = tests
-  .map(test => {
+const testResults = tests.map(test => {
+  try {
     test.actual = replace(...test.args);
-    test.pass = test.actual === test.expected;
-    fs.writeFileSync(__dirname + '/' + test.name + '/actual.txt', test.actual);
+  } catch (err) {
+    console.error(err);
+    test.error = err;
     return test;
-  });
+  }
+  if (typeof test.actual !== 'string') {
+    console.log('replace did not return a sting');
+    return test;
+  }
+  test.pass = test.actual === test.expected;
+  fs.writeFileSync(
+    path.join(__dirname, test.name, 'actual.txt'),
+    test.actual,
+    'utf-8'
+  );
+  return test;
+});
 
 const report = {
-  evaluated: (new Date()).toLocaleString(),
+  evaluated: new Date().toLocaleString(),
   stats: testResults.reduce((stats, test) => {
     stats[test.name] = test.pass ? 'pass' : 'fail';
-    return stats
+    return stats;
   }, {}),
-  tests: testResults
+  tests: testResults,
 };
 
 console.log(JSON.stringify(report.stats, null, '  '));
 
-fs.writeFileSync(__dirname + '/report.json', JSON.stringify(report, null, '  '));
-
-
+fs.writeFileSync(
+  __dirname + '/report.json',
+  JSON.stringify(report, null, '  ')
+);
